df301f18d24ec8bbc44fc2f8284a50dbbee325dc
bartosz <bartosz.brudek@gmail.com>
2025-07-20 19:47:50 +0200

    WebCrawler initial commit

---
+++ WebCrawler/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+StringBuilder builder = new();
+builder.AppendLine("The following arguments are passed:");
+
+var targetSite = args[0]; // first argument for the console app should be the website to crawl.
+
+//Validate argument
+if (!Uri.TryCreate(targetSite, UriKind.Absolute, out var targetSiteUri))
+{
+    Console.WriteLine("Invalid uri provided");
+}
+
+// Setup crawling
+// Figure out the domain, set all bells and whistles
